b00da02a683ab717fb678461b1920c1f289aac6b
MauroGiaccobasso-Git <maurogiaccobasso@gmail.com>
2026-04-28 15:42:52 -0300

    init-project
    Add routes and controllers structure with test endpoint

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
 // Archivo principal del backend.
 // Este archivo funciona como punto de entrada de la aplicación,
 // donde se configurará el servidor, rutas y lógica inicial.
-
 // Importa la librería Express para poder crear el servidor
-const express = require('express');
-
+const express = require("express");
 // Crea una instancia de la aplicación Express
+// Importa las rutas definidas en el archivo homeRoutes,
+// permitiendo utilizarlas dentro del servidor principal
+const homeRoutes = require("./routes/homeRoutes");
 const app = express();
-
 // Define el puerto donde va a correr el servidor
 const PORT = 3000;
-
-// Define una ruta GET para la raíz (/)
-// Cuando alguien entra a http://localhost:3000/
-app.get('/', (req, res) => {
-  // Responde con un mensaje en el navegador
-  res.send('Servidor funcionando');
-});
-
+// Permite manejar datos en formato JSON (por ejemplo en POST)
+app.use(express.json());
+// Conecta las rutas importadas al servidor.
+// Indica que todas las solicitudes a "/" serán manejadas por homeRoutes
+app.use("/", homeRoutes);
 // Inicia el servidor y lo pone a escuchar en el puerto definido
 app.listen(PORT, () => {
   // Muestra un mensaje en la consola indicando que el servidor está corriendo
---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,17 @@
+// Definición de las rutas del sistema.
+// Este archivo se encarga de asociar las URLs con los controladores correspondientes,
+// redirigiendo las solicitudes del usuario hacia la lógica que procesa cada petición.
+// Importa express para poder usar el sistema de rutas
+const express = require("express");
+// Crea un router (objeto que maneja rutas)
+const router = express.Router();
+// Importa la función del controller
+const { getHome, getTest } = require("../controllers/homeController");
+// Define la ruta GET para "/"
+// Cuando alguien accede a "/", se ejecuta getHome
+router.get("/", getHome);
+// Define la ruta GET para "/test"
+// Cuando alguien accede a "/test", se ejecuta la función getTest
+router.get('/test', getTest);
+// Exporta el router para poder usarlo en app.js
+module.exports = router;
