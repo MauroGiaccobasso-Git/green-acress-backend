d9f43bf16949f8d8f7d3a0b55dcc36e5e2a65f8e
MauroGiaccobasso-Git <maurogiaccobasso@gmail.com>
2026-05-07 16:48:22 -0300

    feature/auth-roles
    Added Prisma enum for user roles (ADMIN and SOCIO) and applied database migration. Implemented role-based authorization middleware integrated with JWT authentication flow. Added protected admin route and validated access control using Postman with authenticated ADMIN user.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { login } from "../controllers/authController.js";
-import { verificarToken } from "../middlewares/authMiddleware.js";
+import { verificarToken, autorizarRoles } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -17,6 +17,16 @@ router.get("/perfil", verificarToken, (req, res) => {
 
     // Se devuelve la información del usuario obtenida desde el token
     // (no desde la base de datos, sino desde lo que se guardó en el JWT)
+    usuario: req.usuario,
+  });
+});
+
+// Ruta protegida de prueba solo para administradores
+// Primero valida que exista un token JWT válido
+// Luego valida que el usuario autenticado tenga rol ADMIN
+router.get("/admin", verificarToken, autorizarRoles("ADMIN"), (req, res) => {
+  res.json({
+    message: "Acceso autorizado para administrador",
     usuario: req.usuario,
   });
 });
